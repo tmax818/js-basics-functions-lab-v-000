f7882ae15e8caec4f1d77ff7e4801fc43a56232d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-19 23:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+function distanceTravelledInFeet(start, end) {
+ return Math.abs(start - end) * 264; 
+ 
+}
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
