cebc3ece98982c69bdaaf914e3c1ab212dbbb079
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-20 00:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceTravelledInFeet(start, destination) {
+ return Math.abs(start - destination) * 264; 
+ 
+}
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  const adjDistance = distance - 400;
+  if (distance < 400) {
+  return 0;
+  } else if (distance > 400 && distance < 1999 ) {
+    return (distance - 400) * 0.02
+  } else if (distance > 2000 && distance < 2499 ) {
+    return 25;
+  } else if (distance > 2500) {
+    return 'cannot travel that far';
+  }}
